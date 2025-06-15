508b1c560b9e8fd5f4e56233ecf4d797bf0e54a6
mrunmay-24-ukey <112476865+mrunmay-24-ukey@users.noreply.github.com>
2025-06-15 19:20:30 +0530

    Update server port configuration to a fixed value of 3000

--- index.js
+++ index.js
@@ -295,7 +295,7 @@ app.get('/', (req, res) => {
   res.send('The backend is working fine babygurl');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }); 
